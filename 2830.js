7b51105e48d0615a1c464f49fdb21890f9d5a57c
Matheus Grell <matheus.grell@iteris.com.br>
2019-04-14 14:42:43 -0300

    alteração do desafio 2830

--- 2830.js
+++ 2830.js
@@ -1,22 +1,29 @@
 var input = require('fs').readFileSync('teste.txt', 'utf8');
 var lines = input.split('\r\n');
 
-for (var i = 0; i < lines.length; i++) {
-    if (i % 2 == 0) {
-        var K = parseInt(lines[i]);
-    } else if (i % 2 == 1) {
-        var L = parseInt(lines[i]);
+var K = parseInt(lines[0]);
+var L = parseInt(lines[1]);
+
+if (K && L >= 1 && K && L <= 16 && K != L) {
+
+    if (K % 2 == 1 && L == K + 1 || L % 2 == 1 && K == L + 1) {
+        console.log('oitavas');
+    } else if (((K == 1 || K == 5 || K == 9 || K == 13) && L == K + 2 || L == K + 3)
+        || (K == 2 || K == 6 || K == 10 || K == 14) && L == K + 1 || L == K + 2) {
+        console.log('quartas');
+    } else if (K <= 4 && L >= 5 && L <= 8
+        || L <= 4 && K >= 5 && K <= 8
+        || K <= 12 && K >= 9 && L >= 13 && L <= 16
+        || L <= 12 && L >= 9 && K >= 13 && K <= 16) {
+        console.log('semifinal');
+    } else if (K <= 8 && L >= 8 || L <= 8 && K >= 8) {
+        console.log('final');
     }
+} else {
+    return;
 }
 
-//Final
-if (K <= 8 && L >= 8 || L <= 8 && K >= 8 || K <= 16 && L >= 9 || L <= 16 && K >= 9) {
-    console.log('final');
-} else if (K <= 4 && L >= 5 && L <= 8
-    || L <= 4 && K >= 5 && K <= 8
-    || K <= 12 && K >= 9 && L >= 13 && L <= 16
-    || L <= 12 && L >= 9 && K >= 13 && K <= 16) {
-    console.log('semifinal');
-}else if(K % 2 == 1 && L == K+1 || L % 2 == 1 && K == L+1){
-    console.log('oitavas');
-}
+
+
+
+
